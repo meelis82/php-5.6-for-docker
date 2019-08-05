934d7b9977f604225b22f7eaddb8f3cb0ebab4ca
Meelis Valgeväli <meelis@adm.ee>
2019-08-05 14:28:35 +0300

    Blackfire remove.

--- src/php-5.6/build/modules/build/manifests/php56/extensions.pp
+++ src/php-5.6/build/modules/build/manifests/php56/extensions.pp
@@ -5,7 +5,6 @@ class build::php56::extensions {
   include build::php56::extensions::xdebug
   include build::php56::extensions::memcached
   include build::php56::extensions::redis
-  include build::php56::extensions::blackfire
   include build::php56::extensions::apcu
   include build::php56::extensions::apd
 }
