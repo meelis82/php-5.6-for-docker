8279c8e9adedbacbb0037d59ae71599d4bb455a2
Meelis Valgeväli <meelis@adm.ee>
2019-08-05 15:18:24 +0300

    5.6.40 - ssh2

--- src/php-5.6/build/modules/build/manifests/php56/extensions/ssh2.pp
+++ src/php-5.6/build/modules/build/manifests/php56/extensions/ssh2.pp
@@ -1,18 +1,18 @@
 class build::php56::extensions::ssh2 {
   require build::php56
 
-  file { '/tmp/libssh2-1.9.0.tgz':
+  file { '/tmp/libssh2-1.9.0.tar.gz':
     ensure => present,
-    source => 'puppet:///modules/build/tmp/libssh2-1.9.0.tgz'
+    source => 'puppet:///modules/build/tmp/libssh2-1.9.0.tar.gz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf libssh2-1.9.0.tgz':
-    require => File['/tmp/libssh2-1.9.0.tgz']
+  bash_exec { 'cd /tmp && tar xzf libssh2-1.9.0.tar.gz':
+    require => File['/tmp/libssh2-1.9.0.tar.gz']
   }
 
   bash_exec { 'cd /tmp/libssh2-1.9.0 && ./configure':
     timeout => 0,
-    require => Bash_exec['cd /tmp && tar xzf libssh2-1.9.0.tgz']
+    require => Bash_exec['cd /tmp && tar xzf libssh2-1.9.0.tar.gz']
   }
 
   bash_exec { 'cd /tmp/libssh2-1.9.0 && make':
