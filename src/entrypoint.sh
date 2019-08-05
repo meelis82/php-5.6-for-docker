568e1a6aeebd80c54b57db2c829165c16e4d8db6
Meelis Valgeväli <meelis@adm.ee>
2019-08-05 11:15:15 +0300

    key

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,7 @@
 
 case "${1}" in
   build)
-    /bin/su - root -mc "apt-get update && /src/php-5.6/build.sh && /src/php-5.6/clean.sh"
+    /bin/su - root -mc "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50 && apt-get update && /src/php-5.6/build.sh && /src/php-5.6/clean.sh"
     ;;
   run)
     /bin/su - root -mc "source /src/php-5.6/variables.sh && /src/php-5.6/run.sh"
